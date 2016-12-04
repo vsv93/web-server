2bcc047ab2c230e15b2a6270f7b625496d338d3c
Venkat <vsv_93@yahoo.com>
2016-12-05 01:27:26 +0530

    Updated about us

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleware = {
 // });
 app.use(middleware.logger);
 app.get('/about',middleware.requireAuthentication, function(req, res){
-	res.send('About US!!');
+	res.send('About Us!!');
 })
 app.use(express.static(__dirname +'/public'));
 app.listen(port, function(){
