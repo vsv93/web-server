d051e56beee5f80d9ec87b9c7f812539051010a0
Venkat <vsv_93@yahoo.com>
2016-12-05 12:30:49 +0530

    Port updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js')
 // app.get('/', function(req, res){
